d1ace7c98f81b1af17ea8f4c64654cc4e5706b7d
Andy Speakman <andy.speakman@gmail.com>
2015-02-08 16:07:43 +0000

    Path fixing.
    Updated all references to old folder structure pre-GitHub.

--- database/prepare-tests.sh
+++ database/prepare-tests.sh
@@ -1,1 +1,1 @@
-mysql --user=princes --password=pr1nc35 --database=princes_ppm < /Users/andrewspeakman/Sites/ppm/database/prepare-tests.sql
+mysql --user=princes --password=pr1nc35 --database=princes_ppm < /Users/andrewspeakman/Sites/github/ppm/database/prepare-tests.sql
